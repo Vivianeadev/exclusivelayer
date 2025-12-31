324f01bd03427af6a8fbd567ed585a05d722c295
Vivianeadev <avizaecosystem@gmail.com>
2025-12-31 14:23:46 -0300

    Add initial blockchains data and functions
    Initialize blockchains data structure and related functions.

---
+++ js/blockchains.js
@@ -0,0 +1,18 @@
+// js/blockchains.js
+const blockchains = [
+  { id: 'polygon', name: 'Polygon', symbol: 'MATIC', chainId: 137 },
+  // ... lista completa de 20 blockchains
+];
+
+const otherChainsWallets = {};
+let currentSelectedChain = 'polygon';
+
+function initializeBlockchainButtons() {
+  // Implementação idêntica
+}
+
+function selectBlockchain(chainId) {
+  // Implementação idêntica
+}
+
+// ... outras funções de blockchains
