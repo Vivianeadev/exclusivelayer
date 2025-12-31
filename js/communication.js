b8a17d7b5a66e6db9b2f642c6e0dcd61e46f5bae
Vivianeadev <avizaecosystem@gmail.com>
2025-12-31 14:24:53 -0300

    Add communication.js with initial functions

---
+++ js/communication.js
@@ -0,0 +1,14 @@
+// js/communication.js
+let authorizedChatAddress = null;
+let contacts = [];
+let currentChatContact = null;
+
+function authorizeChatAccess() {
+  // Implementação idêntica
+}
+
+function loadContacts() {
+  // Implementação idêntica
+}
+
+// ... outras funções de comunicação
