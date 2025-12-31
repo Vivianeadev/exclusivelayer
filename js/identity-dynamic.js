297c487e152fd4867c7ba1120dc99dfa7284bdab
Vivianeadev <avizaecosystem@gmail.com>
2025-12-31 14:22:56 -0300

    Add identity-dynamic.js with initial functions

---
+++ js/identity-dynamic.js
@@ -0,0 +1,14 @@
+// js/identity-dynamic.js
+let identityDynamicAuthorized = false;
+let identityDynamicSessionId = null;
+let identityDynamicSignature = null;
+
+function showIdentityAuthorizationInfo() {
+  // Implementação idêntica
+}
+
+async function performIdentityDynamicAuth() {
+  // Implementação idêntica
+}
+
+// ... outras funções do Identity Dynamic
