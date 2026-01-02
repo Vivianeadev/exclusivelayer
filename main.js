249cfa5caca5d81e72d3297f995a952735d87593
Vivianeadev <avizaecosystem@gmail.com>
2026-01-01 22:18:04 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+// VARIÁVEIS GLOBAIS
+const { ethers } = window;
+let currentWallet = null;
+let currentTheme = 'luxury';
+let privateKeyVisible = false;
+let mnemonicVisible = false;
+let mnemonicPhrase = null;
+
+// FUNÇÕES PRINCIPAIS
+function showNotification(message, type = 'info') {
+  // código da função
+}
+
+function toggleTheme() {
+  // código da função
+}
+
+// ... outras funções principais
