6504c1635c824d69dea68710b72ec2e5a3bc85f5
Vivianeadev <avizaecosystem@gmail.com>
2026-01-01 22:18:32 -0300

    Create identity-rpc.js

---
+++ identity-rpc.js
@@ -0,0 +1,13 @@
+// INFRA IDENTITY - RPC CONFIGURAÇÃO
+let currentRpcProvider = 'https://polygon-rpc.com';
+let polygonProvider = null;
+let identityDynamicAuthorized = false;
+
+// FUNÇÕES IDENTITY DYNAMIC
+function showIdentityAuthorizationInfo() {
+  // código da função
+}
+
+async function performIdentityDynamicAuth() {
+  // código da função
+}
